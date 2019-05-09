ac9c0669870b686b224c77262390a4e3ebfa4778
Austin Wong <austinwong@umail.ucsb.edu>
2019-05-08 21:51:56 -0700

    Updates

---
+++ precache-manifest.d6dddfd308333e4169be6fbb2dff1b3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de74e845be604e0c5cb34945dc737b9f",
+    "url": "/pure/index.html"
+  },
+  {
+    "revision": "3882567047bebf5975d8",
+    "url": "/pure/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pure/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3882567047bebf5975d8",
+    "url": "/pure/static/js/main.8482e166.chunk.js"
+  },
+  {
+    "revision": "7cb8f9fd8b9414df4711",
+    "url": "/pure/static/js/runtime~main.45c38e44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pure/static/media/logo.5d5d9eef.svg"
+  }
+]);
